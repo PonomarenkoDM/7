a842c34b987468688526db125fa4a910850bbf50
PonomarenkoDM <159808442+PonomarenkoDM@users.noreply.github.com>
2024-11-20 01:17:51 +0200

    Add files via upload

---
+++ WebApplication3/WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("/result")]
+        public IActionResult Result(string [] Subjects,string [] Assessments, string [] colors)
+        {
+            ViewBag.Subjects = Subjects;
+            ViewBag.Assessments = Assessments;
+            ViewBag.Colors = colors;
+            return View();
+        }
+    }
+}
